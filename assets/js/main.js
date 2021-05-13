b29f45b4a5e6ffdc03d1c8b23a86a7cf40c298b3
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-13 16:29:10 +0100

    Add function to create cards and EventListener to filter them in accordance to select option

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,24 +105,50 @@ const icons = [
  const iconsElem = document.getElementById("icons")
  let iconColour = "black"
 
- icons.forEach(icon => {
-    const {name, prefix, type, family} = icon;
-
-    if (type === 'animal') {
-        iconColour = '#0051ff';
-    } else if (type === 'vegetable') {
-        iconColour = 'orange';
-    } else if (type === 'user') {
-        iconColour = '#7f227f';
-    }
-    
-    iconsElem.insertAdjacentHTML('beforeend', 
+ function createIconCard (name, prefix, family, element) {
+    element.innerHTML += 
     `
     <div class="icon_card d-flex flex-column justify-content-center align-items-center pt-3 pb-2">
         <i class="${family} ${prefix}${name}" style="color: ${iconColour}; font-size: 40px;"></i>
         <span class="text-uppercase my-1">${name}</span>
     </div>
-    `);
+    `;
+}
+ icons.forEach(icon => {
+    const {name, prefix, type, family} = icon;
+
+    if (type === "animal") {
+        iconColour = "#0051ff";
+    } else if (type === "vegetable") {
+        iconColour = "orange";
+    } else if (type === "user") {
+        iconColour = "#7f227f";
+    }
+
+    createIconCard(name, prefix, family, iconsElem);
  });
 
 // Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+const selectElem = document.getElementById("filter");
+
+selectElem.addEventListener("change", function(){
+    iconsElem.innerHTML="";
+    icons.forEach(icon => {
+        const {name, prefix, type, family} = icon;
+    
+        if (type === "animal") {
+            iconColour = "#0051ff";
+        } else if (type === "vegetable") {
+            iconColour = "orange";
+        } else if (type === "user") {
+            iconColour = "#7f227f";
+        }
+    
+        
+        if (type === selectElem.value) {
+            createIconCard(name, prefix, family, iconsElem);
+        } else if (selectElem.value === "All") {
+            createIconCard(name, prefix, family, iconsElem);
+        }
+    });
+})
